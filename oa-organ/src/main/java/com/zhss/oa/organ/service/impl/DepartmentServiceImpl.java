d26744c5499186e8fcfb9a8fa9e80727990ec60d
liyahui <yahui.li@brgroup.com>
2020-08-30 07:58:12 +0800

    finish department module

---
+++ oa-organ/src/main/java/com/zhss/oa/organ/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.zhss.oa.organ.service.impl;
+
+/**
+ * @Description TODO
+ * @Classname DepartmentServiceImpl
+ * @Author
+ * @Date 2020-08-30 07:32
+ * @Version V1.0
+ */
+public class DepartmentServiceImpl {
+}
